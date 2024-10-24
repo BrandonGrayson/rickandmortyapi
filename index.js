f2bcdb00ab340d0e391805bf9d0ec31edfeccb72
Brandon Grayson <brandonbgrayson@gmail.com>
2024-10-23 23:40:12 -0400

    integrated apollo server with express

--- index.js
+++ index.js
@@ -2,11 +2,23 @@
 // const axios = require('axios');
 import express from "express"
 import axios from "axios"
-import {pool} from "./db.js"
+import { pool } from "./db.js"
+import { ApolloServer } from "@apollo/server"
+import { expressMiddleware } from "@apollo/server/express4"
+import { readFile } from 'node:fs/promises'
+import { resolvers } from "./resolvers.js"
+import cors from 'cors'
+
 // import cors from cors
 
 const app = express()
-const port = 3000
+const PORT = 9000
+
+const typeDefs = await readFile('./schema.graphql', 'utf8')
+app.use(cors(), express.json());
+
+const apolloServer = new ApolloServer({typeDefs, resolvers})
+await apolloServer.start()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', async (req, res) => {
@@ -21,7 +33,7 @@ app.get('/', async (req, res) => {
     }
 })
 
-app.get("/characters", async (req, res) => {
+app.use("/characters", expressMiddleware(apolloServer), async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM characters'); // Replace with your table name
         res.json(result.rows);
@@ -31,6 +43,6 @@ app.get("/characters", async (req, res) => {
       }
 })
 
-app.listen(port, () => {
-    console.log(`Rick and Morty app listening on port ${port}`)
+app.listen({ port: PORT }, () => {
+    console.log(`Rick and Morty app listening on port ${PORT}`)
 }) 
